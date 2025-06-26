365a9d601d0e63fb23ae113df99fa80753629fe0
dat120904 <quocdatmast@gmail.com>
2025-06-27 02:18:41 +0700

    Cau hinh ESLint cho du an

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env : {
+        node : true,
+        c0mmonjs : true,
+        es2021 : true,
+    },
+    extends : [
+        'eslint:recommended',
+        'prettier'
+    ],
+};

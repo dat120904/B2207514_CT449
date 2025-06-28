49c4c1be127db39870df101bef00deb2cffbff9f
dat120904 <quocdatmast@gmail.com>
2025-06-29 03:31:41 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -13,4 +15,14 @@ app.get("/", (req, res) => {
 });
 app.use("/api/contacts", contactsRouter);
 
+app.use((req, res, next) => {
+    //Code ở đây sẽ chạy khi không có route được định nghĩa nào khớp 
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status (err.statusCode || 500).json ({
+        message : err.message || "Internal Server Error",
+    });
+});
 module.exports = app;
